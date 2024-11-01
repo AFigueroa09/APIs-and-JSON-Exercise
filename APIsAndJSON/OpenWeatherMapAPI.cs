9df6d79f840f58c728cc0551eadf0d5f6895edf8
Antonio Figueroa <antonio_figueroa@dell.com>
2024-11-01 13:39:06 -0500

    - adds weather api exercise

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        private HttpClient _client;
+        private string _apiKey;
+
+        public OpenWeatherMapAPI(HttpClient client, string apiKey)
+        {
+            _client = client;
+            _apiKey = apiKey;
+            
+        }
+
+        public string GetWeather(string lat, string lon)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={_apiKey}";
+            var weatherResponse = _client.GetStringAsync(url).Result;
+            var currentWeather = JArray.Parse(weatherResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            return currentWeather;
+        }
+
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -6,6 +6,7 @@
         {
             var client = new HttpClient();
             RonVSKanyeAPI api = new RonVSKanyeAPI(client);
+            OpenWeatherMapAPI weatherApi = new OpenWeatherMapAPI(client, "d8bab89e5ff75df864e607006905b22d");
 
             Console.WriteLine("----------------");
             Console.WriteLine("Ron Swanson Quotes");
@@ -14,13 +15,16 @@
                 Console.WriteLine(api.GetRonQuote());
             }
 
+            //Console.WriteLine("----------------");
+            //Console.WriteLine("Kanye Quotes");
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(api.GetKanyeQuote());  
+            //}
+
             Console.WriteLine("----------------");
-            Console.WriteLine("Kanye Quotes");
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(api.GetKanyeQuote());  
-            }
-            
+            Console.WriteLine("Current weather in Round Rock");
+            Console.WriteLine(weatherApi.GetWeather("31.37","10.99"));
         }
     }
 }
